aeffee9d8fbdcacd0c079f41d60cf8c686168a35
sodaler <misha_lector87@mail.ru>
2023-04-13 23:19:20 +0400

    admin lte

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function() {
+    Route::get('/', 'IndexController');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Main'], function() {
-    Route::get('/main', 'IndexController');
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+    Route::group(['namespace' => 'Main'], function() {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Auth::routes();
