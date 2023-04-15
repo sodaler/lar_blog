43f4a95a64ffff52edd1714e6a0935f229232f8a
sodaler <misha_lector87@mail.ru>
2023-04-15 16:17:06 +0400

    Category. Show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Категории</h1>
+                        <h1 class="m-0">{{ $category->title }}</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -26,11 +26,6 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-2">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
-                    </div>
-                </div>
-                <div class="row">
                     <div class="col-6 mt-3">
                         <div class="card">
                             <div class="card-header">
@@ -39,21 +34,15 @@
                             <!-- /.card-header -->
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Название</th>
-                                        <th>Действие</th>
-                                    </tr>
-                                    </thead>
                                     <tbody>
-                                    @foreach($categories as $category)
                                         <tr>
+                                            <td>ID</td>
                                             <td>{{ $category->id }}</td>
-                                            <td>{{ $category->title }}</td>
-                                            <td><a href="{{ route('admin.category.show', compact('category')) }}"><i class="far fa-eye"></i></a></td>
                                         </tr>
-                                    @endforeach
+                                        <tr>
+                                            <td>Название</td>
+                                            <td>{{ $category->title }}</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
