e91eb673d23a82f22a324fb5e41b79cb908c867c
sodaler <misha_lector87@mail.ru>
2023-04-15 16:07:08 +0400

    Category. Index

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Категории</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -29,12 +29,38 @@
                     <div class="col-2">
                         <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
                     </div>
-                    <div class="col-12">
-                        Категории
+                </div>
+                <div class="row">
+                    <div class="col-6 mt-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Responsive Hover Table</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{ $category->id }}</td>
+                                            <td>{{ $category->title }}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
                     </div>
                 </div>
-                <!-- /.row -->
-            </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.row -->
         </section>
         <!-- /.content -->
     </div>
