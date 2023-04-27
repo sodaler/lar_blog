43b83008f72eed8b843a3fc7aa6241561e4712ac
sodaler <misha_lector87@mail.ru>
2023-04-27 22:00:31 +0400

    telescope

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,7 @@ class Post extends Model
     protected $guarded = false;
 
     protected $withCount = ['likedUsers'];
+    protected $with = ['category'];
 
     public function category()
     {
